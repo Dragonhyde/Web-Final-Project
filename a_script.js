206583a7f7c916fcbb957ffa910d7cdeee244473
Dragonhyde <167263337+Dragonhyde@users.noreply.github.com>
2024-12-05 19:27:32 -0700

    About me page added

---
+++ a_script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const aboutText = document.querySelector('.about-text');
+    // Add the fade-in class once the DOM is fully loaded
+    aboutText.classList.add('fade-in');
+});
