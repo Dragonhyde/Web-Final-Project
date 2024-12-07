e4b17519e0f8f150445a815e266c12f9eddc6042
Dragonhyde <167263337+Dragonhyde@users.noreply.github.com>
2024-12-07 15:01:15 -0700

    Update a_script.js

--- a_script.js
+++ a_script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     const aboutText = document.querySelector('.about-text');
-    // Add the fade-in class once the DOM is fully loaded
     aboutText.classList.add('fade-in');
 });
